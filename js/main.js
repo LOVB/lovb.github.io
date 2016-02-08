7efa5e3ae0d87d719a0de6e9eeb3c75904a1f26e
Luis V Ballesteros <lovbstudio@gmail.com>
2016-02-08 13:13:49 +0100

    Initial commit
    Version 1.0

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $(".contacto").hide();
+    $(".boton").click(function(){
+        $('.contacto').fadeToggle();
+    });
+
+    $(window).resize(function(){
+        if ($(window).width() <= 768){
+            $(".contacto").show();
+        } else {
+            $(".contacto").hide();
+        }
+    });
+
+});
