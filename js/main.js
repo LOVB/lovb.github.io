82fdef6aad73056911a685ef1f175fd72f67bd74
Luis V Ballesteros <lovbstudio@gmail.com>
2016-02-09 10:06:33 +0100

    Insertado overflow

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(function(){
-    $(".contacto").hide();
-    $(".boton").click(function(){
-        $('.contacto').fadeToggle();
-    });
-
     $(window).resize(function(){
         if ($(window).width() <= 768){
             $(".contacto").show();
         } else {
             $(".contacto").hide();
         }
+    });
+
+    $(".contacto").hide();
+    $(".boton").click(function(){
+        $('.contacto').fadeToggle();
     });
 
 });
